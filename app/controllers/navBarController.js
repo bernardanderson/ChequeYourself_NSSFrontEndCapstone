9eba752e5c127ac85d2830a3a839e92333577dd5
Bernard Anderson <bernard.anderson@gmail.com>
2016-06-15 18:39:47 -0500

    Added basic NavBar to switch pages A super basic NavBar was added to allow switching between the Ledger and ChequeWriter.  Uses an ng-if rather than routing because I don't like the Url constantly changing.

--- app/controllers/navBarController.js
+++ app/controllers/navBarController.js
@@ -4,14 +4,10 @@ app.controller("navBarController", function($scope){
 
   var navBar = this;
   $scope.navItems = "Basic Navbar";
-  navBar.clicked = true;
+  navBar.clicked = 'appChoice';
 
-  $scope.clickChange = function() {
-    if (navBar.clicked === true) {
-      navBar.clicked = false;
-    } else {
-      navBar.clicked = true;
-    }
+  $scope.clickChange = function(sentPage) {
+    navBar.clicked = sentPage;
   }
 
 });
