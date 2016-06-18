0967956357807b1d39f2417157338a1b5d060379
Bernard Anderson <bernard.anderson@gmail.com>
2016-06-18 12:49:43 -0500

    Got the SpeedDialNavFab Working

--- app/controllers/appChoiceController.js
+++ app/controllers/appChoiceController.js
@@ -2,16 +2,16 @@
 
 app.controller("appChoiceController", function($scope, navBarFactory){
 
-  navBarFactory.setNavButtons([
-    {
-      buttonLabel: "Account Ledger",
-      viewChange: "accountLedger"
-    },
-    {
-      buttonLabel: "Cheque Writer",
-      viewChange: "chequeWriter"
-    }
-  ]);
+  // navBarFactory.setNavButtons([
+  //   {
+  //     buttonLabel: "Account Ledger",
+  //     viewChange: "accountLedger"
+  //   },
+  //   {
+  //     buttonLabel: "Cheque Writer",
+  //     viewChange: "chequeWriter"
+  //   }
+  // ]);
 
   $scope.clickedChoice = function(sentChoice) {
     navBarFactory.setCurrentView(sentChoice);
--- app/controllers/navBarController.js
+++ app/controllers/navBarController.js
@@ -4,11 +4,15 @@ app.controller("navBarController", function($scope, navBarFactory){
 
   $scope.navItems = "Basic Navbar";
 
+  $scope.navButtons = navBarFactory.navButtons;
+
+  $scope.speedDial = {
+    isOpen: false,
+  }
+
   $scope.clickedChoice = function(sentNewView) {
     navBarFactory.setCurrentView(sentNewView);
   }
-
-  $scope.navButtons = navBarFactory.navButtons;
 
   //Watches for any click changes in the current "Main Page" view
   $scope.$watch(function() {return navBarFactory.currentView}, function(newVal, oldVal) {
